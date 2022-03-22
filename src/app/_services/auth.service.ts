9e93ffa1e3e6201735f9b5fc4dc17cc545d3f7c8
kobedesigno <marc.sorhaindo@gmail.com>
2022-03-22 14:13:30 +0100

    user login and signup works!

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -15,7 +15,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
+    return this.http.post(AUTH_API + 'login', {
       username,
       password
     }, httpOptions);
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,6 @@ import { TokenStorageService } from '../_services/token-storage.service';
 })
 export class HeaderComponent implements OnInit {
 
-  private roles: string[] = [];
   isLoggedIn = false;
   showAdminBoard = false;
   showModeratorBoard = false;
@@ -21,10 +20,6 @@ export class HeaderComponent implements OnInit {
 
     if (this.isLoggedIn) {
       const user = this.tokenStorageService.getUser();
-      this.roles = user.roles;
-
-      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
-      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
 
       this.username = user.username;
     }
